99cf1588f2061dad303d9f026ff66a7210e8b5fb
Superbip20 <87235362+SuperWillow@users.noreply.github.com>
2026-02-27 21:36:11 +0600

    Delete js/content.js

--- js/content.js
+++
@@ -1,6 +0,0 @@
-const mutObserver = new MutationObserver(() => {
-    const element = document.querySelector(".bzXtMb.M8OgIe.dRpWwb")
-    if(element) element.remove()
-})
-
-mutObserver.observe(document, { childList: true, subtree: true })
